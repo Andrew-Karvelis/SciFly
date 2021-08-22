9e223faeede27d2807c1728a7afb50efc95e2225
Andrew Karvelis <51180177+Andrew-Karvelis@users.noreply.github.com>
2021-08-22 11:20:55 +1000

    Incomplete endless runner code
    +playerMove, move, killObj, GM c# code
    
    My first attempt at c# code from 2019

---
+++ GM.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GM : MonoBehaviour {
+    //public int zScenePos = 4;
+    public Transform platform;
+    public static bool spawn;
+    public GameObject spawnPos;
+    //public Transform coinObj;
+    //public Transform obstObj;
+    //public Transform capsuleObj;
+    //public int randNum;
+
+    // Use this for initialization
+    void Start () {
+        //Instantiate(platform, new Vector3(0, 0, 4), platform.rotation);
+        //Instantiate(platform, new Vector3(0, 0, 8), platform.rotation);
+        spawn = true;
+    }
+    private void Update()
+    {
+        if (spawn == true)
+        {
+            Instantiate(platform, spawnPos.transform.position, platform.rotation);
+            spawn = false;
+        }
+        Debug.Log(spawn);
+    
+        //if (zScenePos < 120)
+        //{
+            
+        //    zScenePos += 4;
+
+        //}
+
+    }
+      
+}
---
+++ playerMove.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+
+    public Transform player;
+    public float moveSpeed = 3f;
+    private Vector3 target;
+
+    //controls
+    public KeyCode moveL;
+    public KeyCode moveR;
+    public KeyCode moveUL;
+    public KeyCode moveUR;
+    public Transform leftUpPoint;
+    public Transform rightUpPoint;
+
+
+    public float lethal = 0;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if ((Input.GetKeyDown(moveL)) && target != rightUpPoint.position)
+        {
+            if (transform.position.x != 0 && target != leftUpPoint.position)
+            target += Vector3.left;
+
+        }
+
+        else if (Input.GetKeyDown(moveL) && target == rightUpPoint.position)
+        {
+           
+            target += (Vector3.left + Vector3.down);
+        }
+
+        if (Input.GetKeyDown(moveR) && target != leftUpPoint.position)
+        {
+            if (transform.position.x != 1 && target != rightUpPoint.position)
+                target += Vector3.right;
+
+        }
+        else if (Input.GetKeyDown(moveR) && target == leftUpPoint.position)
+        {
+            target += Vector3.right + Vector3.down;
+        }
+
+        if (Input.GetKeyDown(moveUL))
+        {
+            
+            target = leftUpPoint.position;
+
+        }
+        if (Input.GetKeyDown(moveUR))
+        {
+            target = rightUpPoint.position;
+
+
+        }
+        player.transform.position = Vector3.MoveTowards(player.transform.position, target, moveSpeed * Time.deltaTime);
+    }
+
+
+
+}
